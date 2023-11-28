fccd5e8d346b7064ffef40db97356cddcf5e1d2c
Kx09 <karimahsan909@gmail.com>
2023-11-27 19:41:51 -0800

    Two sum in python

---
+++ two_sum_comp_prog.py
@@ -0,0 +1,25 @@
+"""
+
+
+Two-Sum python
+
+two input and outputs sum 
+
+"""
+
+def sum(a,b):
+    output = num1 + num2 
+    return output 
+
+
+
+
+
+
+nums = input("")
+num_list = nums.split(" ")
+num1 = int(num_list[0])
+num2 = int(num_list[1])
+
+print(sum(num1,num2))
+
